244b989da8bd47a728a1afab144b7e5836bf794e
kaa-serpent <guillaumedecadoudal@gmail.com>
2026-08-02 18:52:31 +0200

    Add volumetric XYZA stock simulation

--- src/rotarycam/simulation/__init__.py
+++ src/rotarycam/simulation/__init__.py
@@ -10,10 +10,27 @@ from rotarycam.simulation.cutter_kernel import (
 from rotarycam.simulation.models import SimulationResult
 from rotarycam.simulation.residual import ResidualResult, compute_residual
 from rotarycam.simulation.stock_simulator import removed_volume, simulate_toolpath
+from rotarycam.simulation.volumetric import (
+    ConnectivityStatus,
+    SimulationBudgetExceeded,
+    SimulationIssue,
+    SimulationIssueKind,
+    VolumetricSimulationError,
+    VolumetricSimulationReport,
+    VolumetricSimulationSettings,
+    simulate_volumetric_motion,
+)
 
 __all__ = [
+    "ConnectivityStatus",
     "ResidualResult",
+    "SimulationBudgetExceeded",
+    "SimulationIssue",
+    "SimulationIssueKind",
     "SimulationResult",
+    "VolumetricSimulationError",
+    "VolumetricSimulationReport",
+    "VolumetricSimulationSettings",
     "ball_cutter_surface",
     "compute_residual",
     "cutter_kernel",
@@ -21,5 +38,6 @@ __all__ = [
     "lateral_distance",
     "removed_volume",
     "simulate_toolpath",
+    "simulate_volumetric_motion",
     "tapered_cutter_surface",
 ]
